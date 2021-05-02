0f3e938c3fdd59f4a71f6799624efcc0731dad96
dosabalint <dosa.balint@gmail.com>
2021-05-02 23:18:57 +0200

    009 kész

--- 009--orai-feladat-5/app.asm
+++ 009--orai-feladat-5/app.asm
@@ -2,171 +2,114 @@ Code    Segment
         assume CS:Code, DS:Data, SS:Stack
 
 Start:
-    mov di, 82h
-    mov cx, 10
-    ; jmp Default_hiba
-    
-Keres:
-    mov dl, [di]
-    cmp dl, "/"
-    jz ParamKezdet_elso
-    inc di
-    loop Keres
-    jmp Default
-
-ParamKezdet_elso:
-    inc di
-    mov bl, [di]
-    
-    cmp bl, 63                  ; ?
-    jz Help
-    
-    cmp bl, 48                  
-    jl  Default_hiba            ; kisebb mint 0
-
-    cmp bl, 57                  
-    jg  Default_hiba            ; nagyobb mint 9
-    sub bl, 48
-    
-ParamKezdet_masodik:
-    inc di
-    mov bh, [di]
-
-    cmp bh, 48
-    jl Nincs_masodik
-
-    cmp bh, 57
-    jg Nincs_masodik
-    
-    sub bh, 48
-    mov ax, 10
-    mul bl
-    
-    add al, bh
-    mov cx, ax
-    jmp Init
-    
-Nincs_masodik:
     mov ax, Code
-    mov ds, ax
-
-    mov bh, 0
-    mov cx, bx
-    jmp Init
-
-Help:
-    mov ax, Code
-    mov ds, ax
-
-    mov ax, 3                   ; clear screen
+    mov DS, AX
+    mov dl, 100
+    mov dh, 100
+    push dx
+    mov ax, 13h
     int 10h
-    
-    mov dh, 0                   ; kurzor
-    mov dl, 0
-    mov bh, 0               
-    mov ah, 02h
-    int 10h
- 
-    mov dx, offset helpszoveg      ; gombnyomos szoveg
-    mov ah, 09h
-    int 21h
-
-    jmp Var    
-    
-Default_hiba:
-    mov ax, Code
-    mov ds, ax
-    
-    mov ax, 3                   ; clear screen
-    int 10h
-
-    mov dh, 0                   ; kurzor
-    mov dl, 0
-    mov bh, 0               
-    mov ah, 02h
-    int 10h
-    
-    mov dx, offset hibaszoveg      ; hiba szoveg
-    mov ah, 09h
-    int 21h 
-    
-    xor ax, ax
-    int 16h    
-    
-    mov ax, 3                   ; clear screen
-    int 10h
-    
-Default:
-    mov cx, 10          ; alapertelmezett v0
-
-Init:
-    mov ax, Code
-    mov ds, ax
-
-    xor di, di              ; golyo sor pozicioja
-    xor si, si              ; golyo oszlop pozicioja
-    
-    xor dx, dx
-    push dx                 ; [ido = 0]
-
-Torles:
-    mov ax, 03h
-    int 10h
+    mov ax, 0a000h
+    mov es, ax
+    mov si, 0
 
 Rajz:
-    mov bx, di              ; kurzor
-    mov dh, bl
-    mov bx, si
-    mov dl, bl
-    xor bh, bh
-    mov ah, 02h
-    int 10h
-    
-    mov dx, offset labda    ; labda kirajzolas
-    mov ah, 09h
-    int 21h
+    pop dx
+    xor ah, ah
+    mov al, dh
+    push dx
+    mov bx, 320
+    mul bx
+    pop dx
+    add al, dl
+    jnc Pixel
+    inc ah
 
-    pop ax                  ; sy kiszamitas
-    push ax
-    mov bl, al
-    mul bl
-    shr ax, 1
+Pixel:
+    push dx
     mov di, ax
-
-    pop ax                  ; sx kiszamitas
-    inc ax
-    push ax
-    dec ax
-    mov bl, cl
-    mul bl
-    mov si, ax
-
-    cmp si, 80              ; jobb szele?
-    jnc Var
-
-    cmp di, 25              ; also szele?
-    jnc Var
-    jmp Rajz
+    mov ax, si
+    inc si 
+    inc si
+    mov es:[di], al
 
 Var:
-    xor ax, ax
+    xor ah, ah
     int 16h
+    cmp al, 27
+    jz Program_Vege
+    cmp ah, 75
+    jz Balra
+    cmp ah, 77
+    jz Jobbra
+    cmp ah, 72
+    jz Felfele
+    cmp ah, 80
+    jz Lefele
+    jmp Var
+
+Balra:
+    pop dx
+    dec dl
+    cmp dl, 1
+    ; jnc Tarol1
+    jnc Tarol
+    inc dl
+    jmp Tarol
+    ;Tarol1:
+    ; push dx
+    ; jmp Rajz
+
+Jobbra:
+    pop dx
+    inc dl
+    cmp dl, 250
+    ; jc Tarol2
+    jc Tarol
+    dec dl
+    jmp Tarol
+    ;Tarol2:
+    ; push dx
+    ; jmp Rajz
+
+Felfele:
+    pop dx
+    dec dh
+    cmp dh, 1
+    ; jnc Tarol3
+    jnc Tarol
+    inc dh
+    jmp Tarol
+    ;Tarol3:
+    ; push dx
+    ; jmp Rajz
+
+Lefele:
+    pop dx
+    inc dh
+    cmp dh, 200
+    ; jc Tarol4
+    jc Tarol
+    dec dh
+    jmp Tarol
+    ;Tarol4:
+    ; push dx
+    ; jmp Rajz
+
+Tarol:
+    push dx
+    jmp Rajz
 
 Program_Vege:
-    pop cx
+    mov ax, 03h
+    int 10h
+    pop dx
     mov ax, 4c00h
     int 21h
 
-labda: db "O$"
-helpszoveg: db "A megadott ertek lesz a kezdo sebesseg.$"
-hibaszoveg: db "Hibas kezdeti ertek, defaulttal megyunk tovabb.$"
-nincs_masodik_szoveg: db "Nincs masodik szoveg.$"
-
 Code    Ends
-
 Data    Segment
 Data    Ends
-
 Stack   Segment
 Stack   Ends
-        End Start
+        End     Start
